884c4b083ace018f2779e2e3f76125a10540311e
Marc-Andre Lafortune <github@marc-andre.ca>
2013-03-14 15:02:56 -0400

    Add flattr

--- source/javascripts/custom.js
+++ source/javascripts/custom.js
@@ -33,5 +33,11 @@ for (_i = 0, _len = _ref.length; _i < _len; _i++) {
 
 
 //
+var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
+s.type = 'text/javascript';
+s.async = true;
+s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
+t.parentNode.insertBefore(s, t);
+
 });
 })(jQuery);
